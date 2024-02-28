d96ce0c3c17e8a100413763583089404260669a9
Yasiru Isum Geenula <yigeenuuss425@gmail.com>
2024-02-28 17:53:14 +0530

    Added View btn

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//Project Section See more Button
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("projBtn").addEventListener("click", function() {
         window.open("projects.html", "_blank");
     });
+
+    //Skills Section View CV Button
+    document.getElementById("viewjBtn").addEventListener("click", function() {
+        window.open("resume.html", "_blank");
+    });
 });
+
